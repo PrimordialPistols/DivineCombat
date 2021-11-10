7c83fbf358fd68063c19617e566ac9ae5f25d618
Cynthia Gainey <54631702+crgainey@users.noreply.github.com>
2021-11-09 22:22:31 -0500

    AI Redo

--- DivineCombat/Source/DivineCombat/BaseEnemy.h
+++ DivineCombat/Source/DivineCombat/BaseEnemy.h
@@ -15,23 +15,37 @@ public:
 	// Sets default values for this character's properties
 	ABaseEnemy();
 
-	/** This holds all the stats for the AI */
-	struct FAIStatHolder* StatHolder;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Target Priority")
-	bool bIsWeakAgainst;
-
-	int32 ChanceToHit;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
 
 	/** Used for the Target Priority System Converts the string names into varibles */
 	const FName AxeUnit = "AxeEnemy";
 	const FName SpearUnit = "SpearEnemy";
 	const FName SwordUnit = "SwordEnemy";
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Target Priority") // change where visable
+	bool bIsWeakAgainst;
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+	int32 ChanceToHit;
+
+	/** Methods to calcualte damage and chance to hit */
+	UFUNCTION(BlueprintPure, Category = "Target Priority")
+	int32 ToHit(int BaseNumber, int Hit, int Avoidance, int WeaponAdvantage);
+	UFUNCTION(BlueprintPure, Category = "Target Priority")
+	int32 DamageDealt(int Damage, int PlayerDefense);
+
+	/**Checks if in prioroty before attacking  */
+	UFUNCTION(BlueprintCallable, Category = "Target Priority")
+	void CheckTarget();
+	/** One move to call to calaulate everything */
+	UFUNCTION(BlueprintCallable, Category = "Target Priority")
+	void AttackMove(int BaseNumber, int Hit, int Avoidance, int WeaponAdvantage, int Damage);
+
+	/** Determines if opponent is weak */
+	UFUNCTION(BlueprintCallable, Category = "Target Priority")
+	void WeaponTriangle(int WeaponAdvantage);
 
 public:	
 	// Called every frame
@@ -40,25 +54,5 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	/** Methods to calcualte damage and chance to hit */
-	int32 ToHit();
-	int32 DamageDealt();
-
-	/** Determines if opponent is weak */
-	UFUNCTION(BlueprintCallable, Category = "Target Priority")
-	void WeaponTriangle();
-	/**Checks if in prioroty before attacking  */ 
-	UFUNCTION(BlueprintCallable, Category = "Target Priority")
-	void CheckTarget();
-	/** One move to call to calaulate everything */
-	UFUNCTION(BlueprintCallable, Category = "Target Priority")
-	void AttackMove();
-	/** Will handle movement */
-	UFUNCTION(BlueprintCallable, Category = "Target Priority")
-	void Move();
-
-	UFUNCTION(BlueprintCallable, Category = "Base Enemy")
-	bool TakeDamage(int DamageAmount);
-
-
+	
 };
